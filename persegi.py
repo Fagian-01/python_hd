b7ea4b196d667f2c08089e68ef90a3c0576e6e1d
Fagian-01 <fagian030709@gmail.com>
2024-09-13 09:00:53 +0700

    persegi

---
+++ persegi.py
@@ -0,0 +1,13 @@
+print('='*40)
+print('         Projek Persegi')
+print('='*40)
+
+s = float(input('Masukan Sisi Persegi'))
+
+luas = 4 * s
+keliling = s * s
+
+print('Luas Persegi Tersebut Adalah\t = ' ,luas)
+print('Keliling Persegi Tersebut Adalah\t = ' ,keliling)
+
+
